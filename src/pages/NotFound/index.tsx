ebbb313317d127675c5cdaa066a728c98f2f3b15
seifmegahed <seifmegahed@me.com>
2024-07-03 14:45:52 +0300

    styling update

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,8 @@
 export default function NotFoundPage() {
   return (
-    <>
+    <div className="flex flex-col gap-3 flex-grow items-center justify-center">
       <div className="text-4xl">Oops!</div>
       <div className="text-4xl">Page Not Found</div>
-    </>
+    </div>
   );
 }
